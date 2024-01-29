72f247632ac068c29ca9452b217beae9476a5e4d
ReaperAkira <118540165+ReaperAkira@users.noreply.github.com>
2024-01-29 20:33:10 +0700

    Update and rename update file.py to up_update_file.py

---
+++ up_update_file.py
@@ -0,0 +1,11 @@
+import requests
+
+print("Updating...")
+update = requests.get(
+    "https://raw.githubusercontent.com/ReaperAkira/owo_py/main/update_def.py"
+)
+
+# Save the new version to the local file system
+with open("owo_console\\update_def.py", "wb") as f:
+    f.write(update.content)
+print("Update complete!")
